5e0f4c797bb9ee80df8fd5bbd3ca60852f1453ea
ClaudiuVisan <visanclaudiu2000@tutanota.com>
2022-01-03 11:33:04 +0200

    Deployment changes v2.3

--- MuzicaDB/settings.py
+++ MuzicaDB/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os.path
 from pathlib import Path
 import dj_database_url
-import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -147,6 +146,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-# Activate Django-Heroku.
-django_heroku.settings(locals())
